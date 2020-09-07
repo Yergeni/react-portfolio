d66ec443f04f031bc75280e017626bae299e4531
Yergeni <yergenis@gmail.com>
2020-09-07 19:59:45 -0400

    Updated some changes and information

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,6 +7,7 @@ class About extends Component {
       var name = this.props.data.name;
       var profilepic= "images/"+this.props.data.image;
       var bio = this.props.data.bio;
+      var country = this.props.data.address.country;
       var street = this.props.data.address.street;
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
@@ -32,7 +33,7 @@ class About extends Component {
                   <p className="address">
 						   <span>{name}</span><br />
 						   <span>{street}<br />
-						         {city} {state}, {zip}
+						         {country}. {city} {state}, {zip}
                    </span><br />
 						   <span>{phone}</span><br />
                      <span>{email}</span>
