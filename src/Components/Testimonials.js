db09686d6d131184a29bcaeacf2523931e77d691
Yergeni <yergenis@gmail.com>
2020-10-09 09:09:37 -0400

    Added testimonio from Ale

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -1,38 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Testimonials extends Component {
-  render() {
+	render() {
+		// #11ABB0
+		if (this.props.data) {
+			var testimonials = this.props.data.testimonials.map(function (
+				testimonials
+			) {
+				return (
+					<li key={testimonials.user}>
+						<blockquote>
+							<p>{testimonials.text}</p>
+							<cite>
+								<a style={{ color: "#11ABB0" }} href={testimonials.userLink}>
+									{testimonials.user}
+								</a>
+							</cite>
+						</blockquote>
+					</li>
+				);
+			});
+		}
 
-    if(this.props.data){
-      var testimonials = this.props.data.testimonials.map(function(testimonials){
-        return  <li key={testimonials.user}>
-            <blockquote>
-               <p>{testimonials.text}</p>
-               <cite>{testimonials.user}</cite>
-            </blockquote>
-         </li>
-      })
-    }
+		return (
+			<section id="testimonials">
+				<div className="text-container">
+					<div className="row">
+						<div className="two columns header-col">
+							<h1>
+								<span>Testimonials</span>
+							</h1>
+						</div>
 
-    return (
-      <section id="testimonials">
-      <div className="text-container">
-         <div className="row">
-
-            <div className="two columns header-col">
-               <h1><span>Client Testimonials</span></h1>
-            </div>
-
-            <div className="ten columns flex-container">
-                  <ul className="slides">
-                      {testimonials}
-                  </ul>
-               </div>
-            </div>
-         </div>
-   </section>
-    );
-  }
+						<div className="ten columns flex-container">
+							<ul className="slides">{testimonials}</ul>
+						</div>
+					</div>
+				</div>
+			</section>
+		);
+	}
 }
 
 export default Testimonials;
